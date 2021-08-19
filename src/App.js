59e2e17d40bc694fc3e1c381ec49116910ae59d3
Jane Doe <77185646+gojanedoe@users.noreply.github.com>
2021-08-18 20:30:43 -0400

    Render lists for each category

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import "./App.css";
 import { STARTER_LIST } from "./data/starterList";
 import { CATEGORIES } from "./data/categoriesList";
 
-const App = () => {
+import CategoryList from "./components/CategoryList";
 
+const App = () => {
     return (
         <div className="App">
             <h1>Grocery List</h1>
@@ -12,7 +13,7 @@ const App = () => {
                 {/* TODO: Map over CategoRries to display a <CategoryList> component for each category */}
                 {/* First try to just make the category names appear before implementing <CategoryList> components */}
                 {CATEGORIES.map((category) => (
-                    <p>{category}</p>
+                    <CategoryList key={category} categoryName={category} />
                 ))}
             </div>
         </div>
